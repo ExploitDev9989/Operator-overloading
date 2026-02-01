9a7ba2f7d4e7260c6b271e35b90f83bf22fbe296
ExploitDev9989 <miguelquiroz9989@gmail.com>
2026-02-01 18:07:35 -0500

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_overloading
+{
+    internal class Circle
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public static double operator +(Circle c1, Circle c2)
+        {
+            return c1.Area() + c2.Area();
+        }
+        public static double operator -(Circle c1, Circle c2)
+        {
+            return Math.Abs(c1.Area() - c2.Area());
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Operator_overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(4);
+            Circle c2 = new Circle(7);
+
+            double sum = c1 + c2;
+            double difference = c1 - c2;
+          
+            Console.WriteLine($"Sum of areas: {sum}");
+            Console.WriteLine($"Difference of areas: {difference}");
+        }
+    }
+}
+
